bd43d79f6dca1af0523988b23040f1d87396b5cf
asokolov07011983 <holland48672@gmail.com>
2024-12-04 19:35:01 +0300

    add header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import Logo from '../../../assets/logo.svg'
+import { appData } from "../../constants";
+
 export const Header = () => {
     return (
-        <Logo />
+        <div className="headerContainer">
+            <div>
+                <p className="title">{appData.title}</p>
+                <p  className="developer">Frontend-разработчик: {appData.developer}</p>
+            </div>
+            <Logo />
+        </div>
+
     )
 }
