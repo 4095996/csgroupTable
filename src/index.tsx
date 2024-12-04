1c542472b7ffee66f1218db1b15bffe791f0d1aa
asokolov07011983 <holland48672@gmail.com>
2024-12-04 19:16:48 +0300

    add logo

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store';
-import TableComponent from './components/Table';
+import TableComponent from './components/Table/Table';
 
 import './styles/index.scss';
+import {Header} from "./components/Header";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -13,6 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <Provider store={store}>
+            <Header />
             <TableComponent />
         </Provider>
     </React.StrictMode>
