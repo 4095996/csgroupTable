831e5cd6d623ce06d17336144f24fa76724fca8d
asokolov07011983 <holland48672@gmail.com>
2024-12-05 11:30:47 +0300

    update Table

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -23,6 +23,7 @@ export interface IModifiedColumns {
     key: string;
     title: React.ReactNode;
     render: (text: string | number, record: DataType) => React.ReactNode;
+    width?: number;
 }
 export interface StateProps {
     columns: IModifiedColumns[];
